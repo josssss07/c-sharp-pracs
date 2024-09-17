be44c4b3502e46cd4b3c4b4c9d803f6ffe6468d5
jossss07 <joshuadesai03@gmail.com>
2024-09-17 20:26:35 +0530

    prac1,2 init

---
+++ prac1/prac1/Program.cs
@@ -0,0 +1,55 @@
+﻿namespace prac1
+{
+  class A
+    {
+        //virtual is a starting poitn
+        public virtual void display()
+        {
+            Console.WriteLine("a");
+        }
+    }
+
+    class B : A
+    {
+        //override means next
+        //till the subclass have the overeride method the chain keeps on going 
+        public new virtual void display()
+        {
+            Console.WriteLine("b");
+        }
+
+    }
+
+    class C : B
+    {
+        public override void display()
+        {
+            Console.WriteLine("c");
+        }
+    }
+
+    class D : C
+    {
+        public override void display()
+        {
+            Console.WriteLine("d");
+        }
+    }
+
+    class E : D
+    {
+        public new virtual void display()
+        {
+            Console.WriteLine("E");
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            B a = new E();
+            a.display();    
+        }
+    }
+}
---
+++ prac2/prac2/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace prac2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter the string:");
+            string s1 = Console.ReadLine();
+
+            //1
+            bool b1 = s1.Contains("Test");
+            Console.WriteLine("The string contains 'test' " + b1);
+
+            //2
+            bool b2 = s1.StartsWith("t");
+            Console.WriteLine("The string starts with 't'");
+
+            //3
+            Console.WriteLine("The length of the txt stirng is: " + s1.Length);
+
+            //4
+            Console.WriteLine("the given string in upper case is " + s1.ToUpper() );
+
+            //5
+            Console.WriteLine("enter another string");
+            string s2 = Console.ReadLine();
+            Console.WriteLine(string.Concat(s1, s2));
+
+            //6
+            bool b3 = s2.EndsWith("t");
+            Console.WriteLine("The string ends with 't': " + b3);
+
+            //7
+            Console.WriteLine(s1.Equals(s2));
+
+            //8
+            string s3 = string.Intern(s1);
+            Console.WriteLine("THE ANSWER FOR THIS IS: " + s3);
+
+            //parsing to different data types
+            Console.WriteLine("enter a number: ");
+            int n = int.Parse(Console.ReadLine());
+
+
+        }
+    }
+}
